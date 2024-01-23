67a1a5ec37bed0f8ebcf4639419625d93d24a1c8
Ddggdd135 <1306334428@qq.com>
2024-01-23 21:00:00 +0800

    完成Peer间的连接

--- src/BlessingStudio.Wrap.Client/PeerManager.cs
+++ src/BlessingStudio.Wrap.Client/PeerManager.cs
@@ -1,12 +1,17 @@
 ﻿using BlessingStudio.WonderNetwork;
 using BlessingStudio.WonderNetwork.Events;
 using BlessingStudio.Wrap.Protocol.Packet;
+using BlessingStudio.Wrap.Utils;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using Channel = BlessingStudio.WonderNetwork.Channel;
 
 namespace BlessingStudio.Wrap.Client
 {
@@ -16,6 +21,8 @@ namespace BlessingStudio.Wrap.Client
         public Dictionary<string, long> KeepAliveData = new Dictionary<string, long>();
         public Thread KeepAliveThread;
         public CancellationTokenSource KeepAliveThreadCancellationTokenSource = new();
+        public ushort Nextport = 42000;
+        public IPEndPoint Server = new(new IPAddress(new byte[] { 127, 0, 0, 1 }), 25565);
         public PeerManager()
         {
             KeepAliveThread = new((e) =>
@@ -46,7 +53,51 @@ namespace BlessingStudio.Wrap.Client
             UserManager.AddNewUser(connection, token, ip);
             connection.AddHandler((ChannelCreatedEvent e) =>
             {
-                
+                if (e.Channel.ChannelName.Contains("connection_"))
+                {
+                    Task.Run(() => {
+                        try
+                        {
+                            TcpClient client = new TcpClient();
+                            Channel channel = e.Channel;
+                            new Thread(() =>
+                            {
+                                try
+                                {
+                                    client.Connect(Server);
+                                    while (true)
+                                    {
+                                        byte[] buffer = new byte[1024];
+                                        int c = client.Client.Receive(buffer);
+                                        byte[] bytes = new byte[c];
+                                        Array.Copy(buffer, bytes, c);
+                                        channel.Send(bytes);
+                                        Thread.Sleep(10);
+                                    }
+                                }
+                                catch
+                                {
+                                    client.Close();
+                                    connection.DestroyChannel(channel.ChannelName);
+                                    return;
+                                }
+                            }).Start();
+                            connection.AddHandler((ChannelDeletedEvent e2) =>
+                            {
+                                if (e2.Channel == e.Channel.ChannelName)
+                                {
+                                    client.Close();
+                                }
+                            });
+                        }
+                        catch
+                        {
+
+                        }
+                        }
+                    );
+                    Thread.Sleep(1000);
+                }
             });
             connection.AddHandler((DisposedEvent e) =>
             {
@@ -63,12 +114,63 @@ namespace BlessingStudio.Wrap.Client
                     {
                         KeepAliveData[token] = DateTimeOffset.Now.ToUnixTimeSeconds();
                     }
+                    Debug.WriteLine(token + " Keepalived");
                 }
             });
             lock(KeepAliveData)
             {
                 KeepAliveData[token] = DateTimeOffset.Now.ToUnixTimeSeconds();
             }
+            new Thread(() =>
+            {
+                try
+                {
+                    TcpListener listener = new(new IPAddress(new byte[] {0, 0, 0, 0}), Nextport);
+                    listener.ExclusiveAddressUse = false;
+                    listener.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                    Nextport++;
+                    listener.Start();
+                    while (true)
+                    {
+                        if(UserManager.Find(token) == null)
+                        {
+                            break;
+                        }
+                        TcpClient client = listener.AcceptTcpClient();
+                        string id = new RandomString().Next(0, 8);
+                        Channel channel = connection.CreateChannel($"connection_{id}");
+                        channel.AddHandler((ReceivedBytesEvent e) =>
+                        {
+                            client.Client.Send(e.Data);
+                        });
+                        new Thread(() =>
+                        {
+                            try
+                            {
+                                while (true)
+                                {
+                                    byte[] buffer = new byte[1024];
+                                    int c = client.Client.Receive(buffer);
+                                    byte[] bytes = new byte[c];
+                                    Array.Copy(buffer, bytes, c);
+                                    channel.Send(bytes);
+                                }
+                            }
+                            catch
+                            {
+                                connection.DestroyChannel(channel.ChannelName);
+                                return;
+                            }
+                        }).Start();
+                        Thread.Sleep(10);
+                    }
+                    listener.Stop();
+                }
+                catch
+                {
+                    return;
+                }
+            }).Start();
         }
         public void RemovePeer(string token)
         {
