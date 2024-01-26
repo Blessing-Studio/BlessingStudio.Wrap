f7c52bdcb2c4f4576a5bf0f5e58c1eeec84d5ca5
YangSpring114 <blessing_114514@qq.com>
2024-01-27 00:28:16 +0800

    简化所有特性类

--- src/BlessingStudio.Wrap/Protocol/Attributes/FieldAttribute.cs
+++ src/BlessingStudio.Wrap/Protocol/Attributes/FieldAttribute.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BlessingStudio.Wrap.Protocol.Attributes;
 
-namespace BlessingStudio.Wrap.Protocol.Attributes
-{
-    [AttributeUsage(AttributeTargets.Field)]
-    public class FieldAttribute : Attribute
-    {
-        public int index;
-        public ValueType valueType;
-        public FieldAttribute(int index, ValueType valueType)
-        {
-            this.index = index;
-            this.valueType = valueType;
-        }
+[AttributeUsage(AttributeTargets.Field)]
+public sealed class FieldAttribute : Attribute {
+    public int index;
+
+    public ValueType valueType;
+
+    public FieldAttribute(int index, ValueType valueType) {
+        this.index = index;
+        this.valueType = valueType;
     }
 }
